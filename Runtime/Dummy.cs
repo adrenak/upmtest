1731b41dd9c89529a4a8d9cd57dd6a7401610702
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2022-10-28 15:10:18 +0530

    feat: some new feature to test org publishing

--- Runtime/Dummy.cs
+++ Runtime/Dummy.cs
@@ -12,6 +12,7 @@ namespace Adrenak.UPMTest {
             Debug.Log("Feature 2.0.0");
             Debug.Log("Feature 2.1.0");
             Debug.Log("Another new feature");
+            Debug.Log("Another new feature");
         }
 
         // Update is called once per frame
